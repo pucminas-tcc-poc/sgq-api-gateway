b9d00242b87e053e7c9cffa4662e9722a212803d
Jordyr Caldeira <jordyr.c.v.f@gmail.com>
2022-10-18 00:39:52 -0300

    Configuring API Gateway

--- src/main/java/com/pucminas/sgq/api/gateway/SgqApiGatewayApplication.java
+++ src/main/java/com/pucminas/sgq/api/gateway/SgqApiGatewayApplication.java
@@ -2,10 +2,10 @@ package com.pucminas.sgq.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableEurekaServer
 public class SgqApiGatewayApplication {
 
 	public static void main(String[] args) {
